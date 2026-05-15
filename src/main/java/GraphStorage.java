775e05d7c8725bc9fbf9ce7c6356390dcf2a6c87
LukasAnell <lukasmanell@gmail.com>
2026-05-14 20:44:06 -0700

    Added skeleton for GraphStorage

---
+++ src/main/java/GraphStorage.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+public class GraphStorage {
+
+    /* Sample file format
+     * USERS
+     * 1,Alice,23
+     * 2,Bob,25
+     * 3,Carol,22
+     * CONNECTIONS
+     * 1,2
+     * 1,3
+     *
+     * ---
+     *
+     * USERS: id,name,age
+     * CONNECTIONS: id1,id2
+     */
+
+    public static void saveGraph(Graph graph, String filepath)
+        throws IOException {
+        //
+    }
+
+    public static Graph loadGraph(String filePath) throws IOException {
+        //
+
+        return null;
+    }
+}
