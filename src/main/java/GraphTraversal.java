9454649c264d74fa9422840460655d860518171a
LukasAnell <lukasmanell@gmail.com>
2026-05-14 23:24:40 -0700

    First implementation for GraphTraversal.bfs()

--- src/main/java/GraphTraversal.java
+++ src/main/java/GraphTraversal.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GraphTraversal {
 
     public static List<User> bfs(Graph graph, User start) {
-        //
+        HashMap<User, Boolean> visited = new HashMap<>();
+        List<User> result = new ArrayList<>();
 
-        return null;
+        Queue<User> queue = new LinkedList<>();
+        visited.put(start, true);
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            User currentUser = queue.poll();
+            result.add(currentUser);
+
+            for (User u : graph.getConnections(currentUser)) {
+                if (!visited.containsKey(u)) {
+                    visited.put(u, true);
+                    queue.add(u);
+                }
+            }
+        }
+
+        return result;
     }
 
     public static List<User> dfs(Graph graph, User start) {
