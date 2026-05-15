53647b47f48c2a42ea0444bf7595ad2b70147984
LukasAnell <lukasmanell@gmail.com>
2026-05-14 23:10:18 -0700

    Added skeleton for GraphTraversal

---
+++ src/main/java/GraphTraversal.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class GraphTraversal {
+
+    public static List<User> bfs(Graph graph, User start) {
+        //
+
+        return null;
+    }
+
+    public static List<User> dfs(Graph graph, User start) {
+        //
+
+        return null;
+    }
+
+    public static boolean isConnected(Graph graph, User a, User b) {
+        //
+
+        return false;
+    }
+
+    public static List<User> reachableFrom(Graph graph, User start) {
+        //
+
+        return null;
+    }
+}
