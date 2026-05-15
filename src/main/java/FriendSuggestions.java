d8459d44b70483fb27478340990441e5b2da5540
LukasAnell <lukasmanell@gmail.com>
2026-05-15 14:50:00 -0700

    Add blank skeleton for FriendSuggestions class

---
+++ src/main/java/FriendSuggestions.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class FriendSuggestions {
+
+    public static List<User> suggest(Graph graph, User user) {
+        //
+
+        return null;
+    }
+
+    public static int mutualFriendCount(Graph graph, User a, User b) {
+        //
+
+        return 0;
+    }
+}
