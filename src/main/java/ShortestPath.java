0fcf5d999b465ecb9cf3e9f458b7d969ac1e38a2
LukasAnell <lukasmanell@gmail.com>
2026-05-15 09:31:29 -0700

    Create skeleton for ShortestPath dijkstra: finding path between 2 Users pathWeight, find combined weight of a path

---
+++ src/main/java/ShortestPath.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class ShortestPath {
+
+    public static List<User> dijkstra(Graph graph, User start, User end) {
+        //
+
+        return null;
+    }
+
+    public static int pathWeight(Graph graph, List<User> path) {
+        //
+
+        return 0;
+    }
+}
